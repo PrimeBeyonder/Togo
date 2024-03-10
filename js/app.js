87a2aaa4fa1ee8e1bbd4183c7c4a2221d06f9838
Andrew <thum14252@gmail.com>
2024-03-10 20:19:26 +0630

    todo project setting up

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const width = document.getElementById('innerWidth');
+width.innerHTML = window.innerWidth;
+const height = document.getElementById('innerHeight');
+height.innerHTML = window.innerHeight;
